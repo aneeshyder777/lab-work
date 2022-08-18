a3b6df233d4f672edb10d4740bb709401fbf3672
HP <HP@DESKTOP-F3Q9RRO>
2022-08-18 19:13:20 +0530

    third

---
+++ proj3/src/proj3/Pattern.java
@@ -0,0 +1,20 @@
+//program to print a pattern of numbers.
+
+package proj3;
+
+
+
+public class Pattern {
+
+	public static void main(String[] args) {
+	
+		for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++)
+                System.out.print(j);
+            for (int k = i - 1; k >= 1; k--)
+                System.out.print(k);
+            System.out.println();
+        }
+	}
+
+}
